c10fa72be021d9b7b2918e0f52656754f94a1527
pontazaricardo <pontaza.ricardo@gmail.com>
2017-10-27 11:33:08 +0800

    Update

---
+++ main/Reduction_in_modulo/Modulo/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Xilinx_workspace/GitHub/Reduction_in_modulo/Modulo/test_bench_isim_beh.exe" -prj "C:/Xilinx_workspace/GitHub/Reduction_in_modulo/Modulo/test_bench_beh.prj" "work.test_bench" "work.glbl" 
